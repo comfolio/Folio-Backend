c131d639ccb2cada15791c5782076c6d7129eeec
Edmond Nemsingh <30645192+edmondnemsingh@users.noreply.github.com>
2019-03-30 19:53:47 -0700

    Add files via upload
    Updated Function for Website (Leaner)

--- URLMaker.py
+++ URLMaker.py
@@ -2,23 +2,30 @@ import datetime
 import calendar
 
 
-def URLMaker(dictionary):
+def URLMaker(dictionary, startdate):
+    ##dictionary is the inputted dictionary in {ticker:percent ,...} format
+    ##startdate is the startdate in epoch time.
     #Creating base URL string for call.
     URL = "https://www.blackrock.com/tools/hackathon/portfolio-analysis?calculateExposures=true&calculatePerformance=true&graph=resultMap.PORTFOLIOS%5Bportfolios%5Breturns%5BperformanceChart%5D%5D%5D&identifierType=ticker&positions="
     count = 1
     for key,value in dictionary.items():
-        URL += key
-        URL += "~"
-        URL += str(value)
+        URL += key + "~" + str(value)
         if len(dictionary.items()) != count:
             URL+="%7C"
         count += 1
-    URL += "&startDate="
-    #Initializing separate URL variables.
-    current_time = datetime.datetime.now(datetime.timezone.utc)
-    URL5Y = URL + str(int(current_time.timestamp() - (365 * 24 * 60 * 60 * 5)))
+    URL += "&startDate=" + str(startdate)
+    return(URL)
+    """#Initializing separate URL variables.
+    current_time = datetime.datetime.now(datetime.timezone.utc)"""
+    
+
+    #Not utilized: Portfolio performance across time.
+    """ URL5Y = URL + str(int(current_time.timestamp() - (365 * 24 * 60 * 60 * 5)))
     URL1Y = URL + str(int(current_time.timestamp() - (365 * 24 * 60 * 60)))
     URL6M = URL + str(int(current_time.timestamp() - (182.5 * 24 * 60 * 60 )))
     URL3M = URL + str(int(current_time.timestamp() - (91.25 * 24 * 60 * 60)))
-    URL1M = URL + str(int(current_time.timestamp() - (30.4125 * 24 * 60 * 60)))
-    return URL5Y, URL1Y, URL6M, URL3M, URL1M    
+    URL1M = URL + str(int(current_time.timestamp() - (30.4125 * 24 * 60 * 60))) """
+    #return URL5Y, URL1Y, URL6M, URL3M, URL1M  
+
+#URLMaker({"AAPL":50, "NFLX":25, "FB":20, "MSFT":5}, int(datetime.datetime.now(datetime.timezone.utc).timestamp() - (365 * 24 * 60 * 60)))
+
