2786388b17c968f94ce7de16b0e56ae5e70a193e
Edmond Nemsingh <30645192+edmondnemsingh@users.noreply.github.com>
2019-03-30 22:39:50 -0700

    Add files via upload

--- URLMaker.py
+++ URLMaker.py
@@ -4,6 +4,8 @@ import calendar
 current_time = datetime.datetime.now(datetime.timezone.utc)
 defaultstartdate = int(current_time.timestamp() - (360 * 24 * 60 * 60 * 5))
 def URLMaker(dictionary, startdate = defaultstartdate):
+    """URLMaker is a function that takes a dictionary input in {Ticker:Value,...} format
+    and the start date formatted in epoch time and returns the Blackrock API URL Call."""
     ##dictionary is the inputted dictionary in {ticker:percent ,...} format
     ##startdate is the startdate in epoch time.
     #Creating base URL string for call.
@@ -12,7 +14,7 @@ def URLMaker(dictionary, startdate = defaultstartdate):
     for key,value in dictionary.items():
         URL += key + "~" + str(value)
         if len(dictionary.items()) != count:
-            URL+="%7C"
+            URL += "%7C"
         count += 1
     URL += "&startDate=" + str(startdate)
     return(URL)
@@ -30,3 +32,14 @@ def URLMaker(dictionary, startdate = defaultstartdate):
 
 #URLMaker({"AAPL":50, "NFLX":25, "FB":20, "MSFT":5}, int(datetime.datetime.now(datetime.timezone.utc).timestamp() - (365 * 24 * 60 * 60)))
 
+def URLMakerExposure(dictionary):
+    URL = "https://www.blackrock.com/tools/hackathon/portfolio-analysis?calculateExposures=true&calculatePerformance=true&graph=resultMap.PORTFOLIOS%5Bportfolios%5Bexposures%5BassetClass%5D%5D%5D&identifierType=ticker&includeAllBreakdowns=true&positions="
+    count = 1
+    for key,value in dictionary.items():
+        URL += key + "~" + str(value)
+        if len(dictionary.items()) != count:
+            URL += "%7C"
+        count += 1
+    return URL
+
+print(URLMakerExposure({"MSFT":50, "NFLX":20, "FB":20, "AMZN":10}))
