e156ff42467ad6a53a0bad73e2e2accda0f403fe
ariathefirst <ariang@ucdavis.edu>
2019-03-30 17:04:18 -0700

    make changes to data due to baseline shift from 1 to 0

--- aladdin.py
+++ aladdin.py
@@ -16,14 +16,15 @@ daily_percentage: (cur_percentage - prev_percentage) / prev_percentage
 
 def getGraphVals(json_obj):
 	time_overall_percentage_dict = json_obj['resultMap']['PORTFOLIOS'][0]['portfolios'][0]['returns']['performanceChart']
+
 	epoch_time = [row[0] for row in time_overall_percentage_dict]
 	overall_percentage = [row[1] for row in time_overall_percentage_dict]
-
-	daily_percentage = [(1)]
+	daily_percentage = [(0)]
 	for i in range(1, len(overall_percentage)):
 		daily_percentage.append(overall_percentage[i] - overall_percentage[i-1] / overall_percentage[i-1])
 
 	overall_percentage = [v-1 for v in overall_percentage]
+
 	return epoch_time, time_overall_percentage_dict, overall_percentage, daily_percentage
 
 def getResponse(url = "https://www.blackrock.com/tools/hackathon/portfolio-analysis?calculateExposures=true&calculatePerformance=true&graph=resultMap.PORTFOLIOS%5Bportfolios%5Breturns%5BperformanceChart%5D%5D%5D&identifierType=ticker&positions=VZ~25%7CWMT~25%7CFB~25%7CAAPL~25&startDate=1522400400"):
@@ -32,4 +33,7 @@ def getResponse(url = "https://www.blackrock.com/tools/hackathon/portfolio-analy
 	json_obj = json.loads(t) # string to json, returns a dict of [epoch time, percentage compared to 100%]
 	
 	return getGraphVals(json_obj)
+
+getResponse()
+
 
