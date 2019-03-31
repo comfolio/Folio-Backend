a328608febfba89e6cc8c2953872ff2a65ecb554
ariathefirst <ariang@ucdavis.edu>
2019-03-30 21:07:41 -0700

    add code to get exposure vals

--- aladdin.py
+++ aladdin.py
@@ -11,7 +11,6 @@ Please use 'pip install requests' to add it to your python libraries.
 
 
 """
-epoch_time: time index 
 time_overall_percentage_dict: [d[epoch time, percentage compared to 100%]]
 overall_percentage: percentage changes compared to when market first opens
 daily_percentage: (cur_percentage - prev_percentage) / prev_percentage
@@ -30,13 +29,29 @@ def getPortfolioAnalysis(json_obj):
 
 	return time_overall_percentage_dict, overall_percentage, daily_percentage
 
-def getResponse(url = "https://www.blackrock.com/tools/hackathon/portfolio-analysis?calculateExposures=true&calculatePerformance=true&graph=resultMap.PORTFOLIOS%5Bportfolios%5Breturns%5BperformanceChart%5D%5D%5D&identifierType=ticker&positions=VZ~25%7CWMT~25%7CFB~25%7CAAPL~25&startDate=1522400400"):
+def getExposure(json_obj):
+	# pprint(json_obj)
+	exposures = json_obj['resultMap']['PORTFOLIOS'][0]['portfolios'][0]['exposures']['assetClass']
+	value_list = [d['value'] for d in exposures]
+	y_list = [d['y'] for d in exposures] 
+	
+	print(value_list, y_list)
+	return value_list, y_list
+
+def getResponse(option, url): # option: {1,2} 1:getPortfolioAnalysis called 2: getExposure called
 	portfolioAnalysisRequest = requests.get(url)
 	t = portfolioAnalysisRequest.text # get in text string format, same as json.dumps()
 	json_obj = json.loads(t) # string to json, returns a dict of [epoch time, percentage compared to 100%]
-	
-	return getPortfolioAnalysis(json_obj)
 
-getResponse()
+	if option == 1:
+		res = getPortfolioAnalysis(json_obj)
+	else:
+		res = getExposure(json_obj)	
+
+	return res
+
+# portfolio_analysis = getResponse(1, "https://www.blackrock.com/tools/hackathon/portfolio-analysis?calculateExposures=true&calculatePerformance=true&graph=resultMap.PORTFOLIOS%5Bportfolios%5Breturns%5BperformanceChart%5D%5D%5D&identifierType=ticker&positions=VZ~25%7CWMT~25%7CFB~25%7CVWO~25&startDate=1522400400")
+# exposures = getResponse(2, "https://www.blackrock.com/tools/hackathon/portfolio-analysis?betaPortfolios=SNP500&calculateExposures=true&calculatePerformance=true&calculateRisk=true&calculateStressTests=true&positions=AAPL~25%7CVWO~25%7CAGG~25%7CMALOX~25%7C&riskFreeRatePortfolio=LTBILL1-3M&scenarios=HIST_20081102_20080911%2CHIST_20110919_20110720%2CHIST_20130623_20130520%2CHIST_20140817_20140101%2CUS10Y_1SD%3A%3AAPB%2CINF2Y_1SD%3A%3AAPB%2CUSIG_1SD%3A%3AAPB%2CSPX_1SD%3A%3AAPB%2CDXY_1SD%3A%3AAPB")
+
 
 
